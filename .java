e2f7a6dd0e88f409f5c99f4542f120a836cdeb61
valclorofila <126781465+valclorofila@users.noreply.github.com>
2023-03-04 09:42:19 -0300

    Create .java

---
+++ .java
@@ -0,0 +1,52 @@
+    public class Main {
+    public static void main(String[] args) {
+      while(numerowhile < 3){
+          System.out.println(numerowhile);
+          numerowhile = numerowhile + 1;
+      }
+    }
+    public class Main {
+    public static void main(String[] args) {
+        var numerolf = 0;
+        var numerowhile = 1;
+    if (numerolf > 0){
+        System.out.println("números positivos");
+    }
+    else if(numerolf < 0){
+        System.out.println("números negativos");
+    }
+    else{
+        System.out.println("cero");
+    }
+    do{
+        numerowhile = numerowhile + 1;
+        System.out.println(numerowhile);
+    }while(numerowhile < 2);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        for(var numerofor = 0; numerofor <=3; numerofor = numerofor + 1 ){
+            System.out.println(numerofor);
+        }
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        var Estacion = "invierno";
+        switch(Estacion){
+            case "invierno":
+                System.out.println("es invierno");
+                break;
+            case "verano":
+                System.out.println("es verano");
+                break;
+            case "primavera":
+                System.out.println("es primavera");
+                break;
+            case "otoño":
+                System.out.println("es otoño");
+                break;
+        }
+    }
+}
